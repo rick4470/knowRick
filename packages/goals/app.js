5b8f3e682c95086ba85d495d0957ffd8f70d48a3
Rick <rick@saltyslopes.com>
2015-07-05 19:27:57 -0600

    created a better experience for viewing public goals

--- packages/goals/app.js
+++ packages/goals/app.js
@@ -7,21 +7,21 @@ var Module = require('meanio').Module;
 
 var Goals = new Module('goals');
 
-Goals.register(function(app, auth, database) {
-  Goals.routes(app, auth, database);
-  Goals.aggregateAsset('css', 'goals.css');
+Goals.register(function (app, auth, database) {
+    Goals.routes(app, auth, database);
+    Goals.aggregateAsset('css', 'goals.css');
 
-  // Lib Aggrgated Files Javascript
-  var libDir = '../lib/';
-  Goals.aggregateAsset('js', libDir + 'angular-touch/angular-touch.js');
-  Goals.aggregateAsset('js', libDir + 'venturocket-angular-slider/build/angular-slider.min.js');
-  Goals.aggregateAsset('js', libDir + 'angular-timer/dist/angular-timer.js');
-  
-  // Make sure that angular package knows about any injections.
-  Goals.angularDependencies([
+    // Lib Aggrgated Files Javascript
+    var libDir = '../lib/';
+    Goals.aggregateAsset('js', libDir + 'angular-touch/angular-touch.js');
+    Goals.aggregateAsset('js', libDir + 'venturocket-angular-slider/build/angular-slider.min.js');
+    Goals.aggregateAsset('js', libDir + 'angular-timer/dist/angular-timer.js');
+
+    // Make sure that angular package knows about any injections.
+    Goals.angularDependencies([
     'vr.directives.slider',
     'timer'
   ]);
-  
-  return Goals;
+
+    return Goals;
 });
--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -1,27 +1,40 @@
 'use strict';
 
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global', '$http','Goals',
-  function($scope, Global, $http, Goals) {
-    $scope.oneAtATime = true;
-    $scope.open = false;
-    $scope.global = Global;
+angular.module('mean.system').controller('IndexController', ['$scope', 'Global', '$http', 'Goals',
+  function ($scope, Global, $http, Goals) {
+        $scope.oneAtATime = true;
+        $scope.open = false;
+        $scope.global = Global;
 
-    $scope.pageInit = function () {
-      $http.get('/latest-page').success(function(data) {
-        $scope.page = data;
-      });
-      Goals.query(function(data) {
-        $scope.goals = data;
-      });
-    };
+        $scope.pageInit = function () {
+            $http.get('/latest-page').success(function (data) {
+                $scope.page = data;
+            });
+            Goals.query(function (data) {
+                $scope.goals = data;
+                for (var goal in $scope.goals) {
+                    if (!isNaN(goal)) {
+                        $scope.goals[goal].isOpen = false;
+                        $scope.goals[goal].chevron = 'glyphicon-chevron-right';
+                    }
+                }
+                window.setTimeout(function () {
+                    $scope.$apply(function () {
+                        $scope.goals[0].isOpen = true;
+                        $scope.goals[0].chevron = 'glyphicon-chevron-down';
+                    });
+                }, 1000);
+            });
+        };
 
-    $scope.viewSubGoal = function(goal){
-      $scope.open = true;
-      if (goal.subGoal.length > 0) {
-        $scope.subGoals = goal.subGoal;
-        $scope.goalName = goal.name;
-        $scope.completeBy = goal.completeBy;
-      }
-    };
-  }
-]);
+        $scope.chevron = function (goal) {
+            switch (goal.chevron) {
+                case 'glyphicon-chevron-down':
+                    goal.chevron = 'glyphicon-chevron-right';
+                    break;
+                case 'glyphicon-chevron-right':
+                    goal.chevron = 'glyphicon-chevron-down';
+                    break;
+            }
+        };
+}]);
